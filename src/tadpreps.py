55a397a205c752cf3ad016ee992bc0c9793b66d0
don-c-smith <don.cs.smith@gmail.com>
2025-01-03 09:45:50 -0600

    Added src folder and uploaded main tadpreps.py file

---
+++ src/tadpreps.py
@@ -0,0 +1,10 @@
+"""
+This is the TADPREPS codebase. All core functions will be defined internally (so no additional files are necessary at
+runtime) and the logging file will be created in the same working directory as the script itself.
+"""
+
+# Library imports
+import numpy as np
+import pandas as pd
+import pathlib as path
+import seaborn as sns
