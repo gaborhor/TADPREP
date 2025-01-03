f8db54027a64e341ffb461c87c1a3fa09227bf6d
don-c-smith <don.cs.smith@gmail.com>
2025-01-03 10:38:14 -0600

    Added new code

--- src/tadpreps.py
+++ src/tadpreps.py
@@ -6,7 +6,21 @@ runtime) and the logging file will be created in the same working directory as t
 # Library imports
 import numpy as np
 import pandas as pd
-import pathlib as path
+from pathlib import Path
 import seaborn as sns
 import logging
 import sys
+
+# Set up error logging
+# We persist with "%-type" formatting to preserve backward compatibility
+logging.basicConfig(
+    level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s',
+    handlers=[logging.FileHandler('tadpreps_runtime_log.log'), logging.StreamHandler(sys.stdout)]
+)
+
+logger = logging.getLogger(__name__)
+logger.info('Initiating TADPREPS...')
+
+
+def load_file():
+    pass
