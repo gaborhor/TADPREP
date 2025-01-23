35f08d13c4737d5ecbd88431836f8ae3089aeebf
don-c-smith <don.cs.smith@gmail.com>
2025-01-23 13:43:47 -0600

    Added top-level __init__.py file

---
+++ src/tadpreps/__init__.py
@@ -0,0 +1,37 @@
+"""TADPREPS - Interactive Data Preparation Tool"""
+
+# Import public-facing methods that users will access
+from .package import (
+    prep_df,
+    info,
+    reshape,
+    rename_and_tag,
+    feature_stats,
+    impute,
+    encode_and_scale
+)
+
+# Define exposure if "from tadpreps import *" is called
+__all__ = [
+    'prep_df',
+    'info',
+    'reshape',
+    'rename_and_tag',
+    'feature_stats',
+    'impute',
+    'encode_and_scale',
+]
+
+# Package metadata
+__version__ = '0.1.0'
+__author__ = "Donald Smith"
+
+
+def main():
+    """Entry point for running the full interactive version of the pipeline"""
+    from .tadpreps_interactive import main as interactive_main
+    interactive_main()
+
+
+if __name__ == "__main__":
+    main()
