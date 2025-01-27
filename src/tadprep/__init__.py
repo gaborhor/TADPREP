5b21996c5158a966b620e0df171193689936f065
don-c-smith <don.cs.smith@gmail.com>
2025-01-27 10:25:42 -0600

    Updated __init__.py

--- src/tadprep/__init__.py
+++ src/tadprep/__init__.py
@@ -3,7 +3,7 @@
 # Import public-facing methods that users will access
 from .package import (
     prep_df,
-    info,
+    file_info,
     reshape,
     rename_and_tag,
     feature_stats,
@@ -14,7 +14,7 @@ from .package import (
 # Define exposure if "from tadprep import *" is called
 __all__ = [
     'prep_df',
-    'info',
+    'file_info',
     'reshape',
     'rename_and_tag',
     'feature_stats',
