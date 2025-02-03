7a988f761b44849b4c65d1cf3e18231bb827c14e
don-c-smith <don.cs.smith@gmail.com>
2025-02-03 11:49:44 -0600

    Updated top-level __init__.py file for TADPREP library

--- src/tadprep/__init__.py
+++ src/tadprep/__init__.py
@@ -2,26 +2,26 @@
 
 # Import public-facing methods that users will access
 from .package import (
-    prep_df,
     file_info,
     reshape,
     rename_and_tag,
     feature_stats,
     impute,
     encode,
-    scale
+    scale,
+    prep_df
 )
 
 # Define exposure if "from tadprep import *" is called
 __all__ = [
-    'prep_df',
     'file_info',
     'reshape',
     'rename_and_tag',
     'feature_stats',
     'impute',
     'encode',
-    'scale'
+    'scale',
+    'prep_df'
 ]
 
 # Package metadata
