a3fc2dbeb76437917338b10397bc102f2fb3dd91
don-c-smith <don.cs.smith@gmail.com>
2025-02-04 10:17:23 -0600

    Completed tests for file_info and reshape methods

--- dev_test.py
+++ dev_test.py
@@ -5,6 +5,17 @@ import tadprep as tp  # Testing package-level import - I want TADPREP to mirror 
 df_raw = pd.read_csv(r"C:\Users\doncs\Documents\GitHub\TADPREP\data\sample_data_longlean.csv")
 
 # Checking import
-print(df_raw)
+# print(df_raw)
 
+# Testing file_info method
+# tp.file_info(df_raw, verbose=False)
+# tp.file_info(df_raw, verbose=True)
 
+# After debugging, I am satisfied with the operation of the file_info method
+
+# Testing reshape method
+# df_reshape = tp.reshape(df_raw, verbose=False)
+# df_reshape = tp.reshape(df_raw, verbose=True)
+# print(df_reshape)
+
+# After debugging, I am satisfied with the operation of the reshape method
