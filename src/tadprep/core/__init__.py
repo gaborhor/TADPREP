7c2f56e8a605ff5b281d72cd4fb7a048a87ee730
don-c-smith <don.cs.smith@gmail.com>
2025-02-07 09:42:20 -0600

    Updated __init__.py

--- src/tadprep/core/__init__.py
+++ src/tadprep/core/__init__.py
@@ -1,7 +1,7 @@
 """Core data mutation functions for TADPREP"""
 
 from .transforms import (
-    _file_info_core,
+    _df_info_core,
     _reshape_core,
     _rename_and_tag_core,
     _feature_stats_core,
@@ -12,7 +12,7 @@ from .transforms import (
 )
 
 __all__ = [
-    '_file_info_core',
+    '_df_info_core',
     '_reshape_core',
     '_rename_and_tag_core',
     '_feature_stats_core',
